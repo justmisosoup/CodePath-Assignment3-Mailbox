2b9e413a56ebdaa6b9a3391d4c47038ed24af0f6
Sara Menefee <s.menefee@me.com>
2014-09-29 16:33:27 -0700

    Mailbox CodePath

---
+++ CodePath - Mailbox/ViewController.swift
@@ -0,0 +1,142 @@
+//
+//  ViewController.swift
+//  CodePath - Mailbox
+//
+//  Created by Sara  on 9/25/14.
+//  Copyright (c) 2014 Sara Menefee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // Outlets
+
+    
+    
+    @IBOutlet weak var deleteImg: UIImageView!
+    @IBOutlet weak var archiveImg: UIImageView!
+    @IBOutlet weak var laterImg: UIImageView!
+    @IBOutlet weak var listImg: UIImageView!
+    @IBOutlet weak var msgImg: UIImageView!
+    @IBOutlet weak var msgImgView: UIView!
+    @IBOutlet weak var navImgView: UIImageView!
+    @IBOutlet weak var feedView: UIView!
+    @IBOutlet weak var zeroImage: UIImageView!
+    @IBOutlet weak var searchImage: UIImageView!
+    @IBOutlet weak var feedImage: UIImageView!
+    @IBOutlet weak var feedScrollView: UIScrollView!
+    
+    var msgImgCenter: CGPoint!
+    var laterX: CGFloat!
+    var archiveX: CGFloat!
+    var msgLocationX: CGFloat!
+
+    // Set colours for UIColor
+    
+    let gray = UIColor(red: 227/255, green: 227/255, blue: 227/255, alpha: 1)
+    let brown = UIColor(red: 215/255, green: 165/255, blue: 120/255, alpha: 1)
+    let green = UIColor(red: 116/255, green: 215/255, blue: 104/255, alpha: 1)
+    let red = UIColor(red: 233/255, green: 85/255, blue: 59/255, alpha: 1)
+    let yellow = UIColor(red: 249/255, green: 210/255, blue: 70/255, alpha: 1)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Take feedScrollView and set the content size to the 'feedView' UIView size (width + height)
+        
+        feedScrollView.contentSize = feedView.frame.size
+        
+        laterImg.alpha = 0
+        archiveImg.alpha = 0
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+        
+    }
+    
+    // Pan Gesture of element
+
+  
+    @IBAction func onPanMsg(gestureRecognizer: UIPanGestureRecognizer) {
+        
+        // Variables
+        
+        var location = gestureRecognizer.locationInView(view)
+        var translation = gestureRecognizer.translationInView(view)
+        var velocity = gestureRecognizer.velocityInView(view)
+        
+        // Beginning of Pan Gesture
+        
+        if gestureRecognizer.state == UIGestureRecognizerState.Began {
+            laterX = laterImg.frame.origin.x
+            archiveX = laterImg.frame.origin.x
+            
+            UIView.animateWithDuration(0.75, animations: { () -> Void in
+                self.laterImg.alpha = 1
+                self.archiveImg.alpha = 1
+            })
+            
+        }
+            
+            
+            // Change in Pan Gesture
+            
+        else if gestureRecognizer.state == UIGestureRecognizerState.Changed {
+            
+            msgLocationX = gestureRecognizer.translationInView(msgImgView).x
+            msgImg.frame.origin.x = msgLocationX
+            
+            if (-160 <= msgLocationX) && (msgLocationX < -60) {
+                msgImgView.backgroundColor = yellow
+                laterImg.frame.origin.x = laterX + msgLocationX + 60
+            }
+            
+            if (-320
+                <= msgLocationX) && (msgLocationX < -160) {
+                    msgImgView.backgroundColor = brown
+                    laterImg.frame.origin.x = laterX + msgLocationX + 60
+            }
+            
+            if (260 <= msgLocationX) && (msgLocationX < 60) {
+                msgImgView.backgroundColor = green
+                laterImg.frame.origin.x = laterX + msgLocationX + 60
+            }
+            
+            if (320
+                <= msgLocationX) && (msgLocationX < 160) {
+                    msgImgView.backgroundColor = red
+                    laterImg.frame.origin.x = laterX + msgLocationX + 60
+            }
+            
+            
+        }
+            
+            // End of Pan Gesture
+            
+        else if gestureRecognizer.state == UIGestureRecognizerState.Ended {
+            UIView.animateWithDuration(0.5, animations: { () -> Void in
+                if velocity.x >= self.msgImg.center.x + 260 {
+                    self.msgImg.center.x = 320
+                    self.deleteImg.alpha = 0
+                }
+                    
+                else if velocity.x <= self.msgLocationX {
+                    self.msgImg.center.x = -320
+                    self.laterImg.alpha = 0
+                }
+            })
+            
+            
+        }
+        
+    }
+    
+    
+    
+    
+}
